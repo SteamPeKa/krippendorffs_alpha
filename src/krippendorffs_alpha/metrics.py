7ed676c3bc1f2aca5dd3bbe001e0e590b98c9603
SteamPeKa <ya.steampeka@ya.ru>
2020-11-02 22:51:18 +0300

    Just found out that Idea making strange commits (wrong credentials). Making some tweaks in git config.

--- src/krippendorffs_alpha/metrics.py
+++ src/krippendorffs_alpha/metrics.py
@@ -8,19 +8,19 @@ from typing import TypeVar, Generic, Iterable, Union
 
 import numpy
 
-T = TypeVar("T")
+VT = TypeVar("VT")
 
 
-class AbstractMetric(Generic[T], object):
+class AbstractMetric(Generic[VT], object):
     def __init__(self, *args, **kwargs):
         return
 
-    def __call__(self, value1: T, value2: T) -> float:
+    def __call__(self, value1: VT, value2: VT) -> float:
         raise NotImplementedError(
             "Abstract method of class AbstractMetric call from class {}".format(self.__class__.__name__)
         )
 
-    def get_metric_tensor(self, possible_values: Iterable[T], symmetric=True):
+    def get_metric_tensor(self, possible_values: Iterable[VT], symmetric=True):
         if symmetric:
             return numpy.array(
                 [[self(row_value, column_value) for column_value in possible_values] for row_value in possible_values]
@@ -31,18 +31,18 @@ class AbstractMetric(Generic[T], object):
                                 for c, row_value in enumerate(possible_values)])
 
 
-class NominalMetric(AbstractMetric):
-    def __call__(self, value1: T, value2: T) -> float:
+class NominalMetric(AbstractMetric[VT]):
+    def __call__(self, value1: VT, value2: VT) -> float:
         return 0.0 if value1 == value2 else 1.0
 
 
-class IntervalMetric(AbstractMetric):
-    def __call__(self, value1: T, value2: T) -> float:
+class IntervalMetric(AbstractMetric[VT]):
+    def __call__(self, value1: VT, value2: VT) -> float:
         return (value1 - value2) ** 2
 
 
-class RatioMetric(AbstractMetric):
-    def __call__(self, value1: T, value2: T) -> float:
+class RatioMetric(AbstractMetric[VT]):
+    def __call__(self, value1: VT, value2: VT) -> float:
         if value1 == value2:
             return 0.0
         return ((value1 - value2) / (value1 + value2)) ** 2
